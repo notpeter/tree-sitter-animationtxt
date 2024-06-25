38c7c0fbdeb4d05d51f3d387fc9df3607af75205
Peter Tripp <petertripp@gmail.com>
2024-06-25 01:30:44 -0400

    Making leading whitespace error.

--- grammar.js
+++ grammar.js
@@ -4,23 +4,56 @@ module.exports = grammar({
   name: "playdateanimation",
   rules: {
     animationtxt: ($) => repeat(choice($.loopCount, $.frames, $.introFrames)),
-    loopCount: ($) => seq("loopCount", "=", $.integer, $.newline),
-    frames: ($) => seq("frames", "=", $.framelist, $.newline),
-    introFrames: ($) => seq("introFrames", "=", $.framelist, $.newline),
-    framelist: ($) => seq($.frameLiteral, repeat(seq(",", $.frameLiteral))),
-    frameLiteral: ($) => {
-      const positiveInt = /[1-9][0-9]*/;
-      const frameRepeat = seq(
-        positiveInt, // frameId
-        token.immediate("x"),
-        token.immediate(positiveInt), // frameCount
+    loopCount: ($) =>
+      seq(
+        "loopCount",
+        optional(/ +/),
+        "=",
+        optional(/ +/),
+        $.positiveInt,
+        optional(/ +/),
+        $.newline,
+      ),
+    frames: ($) =>
+      seq(
+        "frames",
+        optional(/ +/),
+        "=",
+        optional(/ +/),
+        $.framelist,
+        optional(/ +/),
+        $.newline,
+      ),
+    introFrames: ($) =>
+      seq(
+        "introFrames",
+        optional(/ +/),
+        "=",
+        optional(/ +/),
+        $.framelist,
+        optional(/ +/),
+        $.newline,
+      ),
+    framelist: ($) =>
+      seq(
+        choice($.frameNonRepeat, $.frameRepeat),
+        repeat(
+          seq(",", optional(/ +/), choice($.frameNonRepeat, $.frameRepeat)),
+        ),
+      ),
+    frameNonRepeat: ($) => token(/[1-9][0-9]*/),
+    frameRepeat: (_) => {
+      return token(
+        seq(
+          /[1-9][0-9]*/,
+          token.immediate("x"),
+          token.immediate(/[1-9][0-9]*/),
+        ),
       );
-      return token(choice(prec(1, frameRepeat), prec(2, positiveInt)));
     },
-    frameId: ($) => $.positiveInt,
-    frameCount: ($) => $.positiveInt,
     positiveInt: (_) => /[1-9][0-9]*/,
-    integer: (_) => /[0-9]+/,
     newline: (_) => /(\r?\n)/,
+    w: (_) => / +/,
   },
+  extras: (_) => [],
 });
---
+++ queries/highlights.scm
@@ -0,0 +1,9 @@
+["="] @operator
+[
+    "loopCount"
+    "frames"
+    "introFrames"
+] @keyword
+(positiveInt) @number
+(frameNonRepeat) @number
+(frameRepeat) @string
